4f0b960963efba8e13f5aab6169d5e622bf963d1
minjeadev <93039870+minjeadev@users.noreply.github.com>
2023-05-26 00:17:48 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "jaeyeong.h"
 
 #define READ_SIZE 512
+
+int error(const char* format, ...);
+void replace(char *str, char find, char replace);
+char** split(char *str, const char *delimiter, int *count);
+
+int main(int argc, char* argv[]) {
+  unsigned int size = 0, length = 0;
+  char *version = "v0.1.0", *buffer = NULL, *fname = NULL;
+	FILE *file = NULL;
+  if (argc < 2) return error("Usage: %s <filename>\n", argv[0]);
+  file = fopen(argv[1], "r");
+  if (file == NULL) return error("Error: Unable to open file %s\n", argv[1]);
+  while (!feof(file)) {
+		size += READ_SIZE;
+		buffer = realloc(buffer, sizeof(char) * size);
+		length += fread((buffer + size) - READ_SIZE, 1, READ_SIZE, file);
+  }
+  if (fclose(file) != 0) {
+    if (buffer) free(buffer);
+    return error("Error: Unable to close file %s\n", argv[1]);
+  }
+  if (!buffer) return 1;
+	buffer[length] = '\0';
+  replace(buffer, ';', '\n');
+  int tc, last = 0;
+  char **lines = split(buffer, "\n", &tc);
+  for (int i = 0; i < tc; i++) if (lines[i] != '\0') last = 1;
+  if (lines[0] != CODE_START || lines[last] != CODE_END) {
+    if (buffer) free(buffer);
+    if (lines) free(lines);
+    return error("Error: Invalid file format\n");
+  }
+  free(lines);
+  return 0;
+}
+
+int error(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  return 1;
+}
 
 void replace(char *str, char find, char replace) {
   while(*str) {
@@ -27,48 +71,4 @@ char** split(char *str, const char *delimiter, int *count) {
   }
   *count = tokenCount;
   return tokens;
-}
-
-int main(int argc, char* argv[]) {
-  unsigned int size = 0;
-	unsigned int length = 0;
-  char *version = "v0.1.0";
-  char *buffer = NULL;
-  char *fname = NULL;
-	FILE *file = NULL;
-  if (argc < 2) {
-    printf("Usage: %s <filename>\n", argv[0]);
-    return 1;
-  }
-  file = fopen(argv[1], "r");
-  if (file == NULL) {
-    printf("Error: Unable to open file %s\n", argv[1]);
-    return 1;
-  }
-  while (!feof(file)) {
-		size += READ_SIZE;
-		buffer = realloc(buffer, sizeof(char) * size);
-		length += fread((buffer + size) - READ_SIZE, 1, READ_SIZE, file);
-  }
-  if (fclose(file) != 0) {
-    printf("Error: Unable to close file %s\n", argv[1]);
-    if (buffer) free(buffer);
-    return 1;
-  }
-  if (!buffer) return 1;
-	buffer[length] = '\0';
-
-  replace(buffer, ';', '\n');
-  int tc, last = 0;
-  char **lines = split(buffer, "\n", &tc);
-  for (int i = 0; i < tc; i++)
-    if (lines[i] != '\0') last = 1;
-  if (lines[0] != CODE_START || lines[last] != CODE_END) {
-    printf("Error: Invalid file format\n");
-    if (buffer) free(buffer);
-    if (lines) free(lines);
-    return 1;
-  }
-  free(lines);
-  return 0;
 }
